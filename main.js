66e5b59e8d99e440b8876562507171d6d9a94bba
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-04-10 14:07:02 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+console.log("IN HERE!");
+
+// DATA
+// CHALLENGE: MOVE TO SEPARATE FILE (EXPORT/IMPORT)
+const data = "";
+
+// reuseable function to render HTML on the DOM
+// CHALLENGE: MOVE TO SEPARATE FILE (EXPORT/IMPORT)
+const renderToDom = (divId, html) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = html;
+}
+
+// HTML FORM COMPONENT
+// CHALLENGE: MOVE TO SEPARATE FILE (EXPORT/IMPORT)
+const formHtml = () => {
+ 
+}
+
+// HTML RIDDLE COMPONENT
+// CHALLENGE: MOVE TO SEPARATE FILE (EXPORT/IMPORT)
+const riddle = (array) => {
+ 
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  
+}
+
+// CAPTURE EVENTS ON THE DOM
+const events = () => {
+ 
+}
+
+// INITIALIZE THE APP
+const init = () => {
+ 
+}
+
+init();
